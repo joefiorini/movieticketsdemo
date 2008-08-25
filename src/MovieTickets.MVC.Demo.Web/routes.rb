c56c57482e370f6ef6298d01aee2e92c0eeac6b1
unknown <joe@.(none)>
2008-08-24 21:57:36 -0400

    Added movies nav item

--- src/MovieTickets.MVC.Demo.Web/routes.rb
+++ src/MovieTickets.MVC.Demo.Web/routes.rb
@@ -3,6 +3,9 @@
 $routes.map_route "Movie", "movies/{id}",
   {:controller => 'Movies', :action => 'Show' }
 
+$routes.map_route "Movies", "movies",
+  {:controller => 'Movies', :action => 'Index' }
+
 $routes.map_route "{controller}/{action}/{id}",
   {:controller => 'Home', :action => 'index', :id => ''}
 
